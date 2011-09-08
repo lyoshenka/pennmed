c281b31fe68477f2c71ca7142200747f4df94df7
Alex Grintsvayg <lyoshenka@gmail.com>
2011-09-08 09:44:26 -0400

    whitespace

--- pennmed2.js
+++ pennmed2.js
@@ -9,7 +9,7 @@ if (!match)
 
 var req = new XMLHttpRequest();
 req.onreadystatechange = function() {
-  if(req.readyState==4 && req.status==200) {   
+  if(req.readyState==4 && req.status==200) {
     var match2 = /\"(mms:[^"]+)\"/.exec(req.responseText);
     if (!match2)
     {
